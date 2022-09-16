72aba5565edd26e087ca15a56cb881ad928a8f5b
Andrew Kelly <andy@mx51.io>
2022-09-17 01:23:26 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require("@actions/core");
+
+const file = core.getInput("file", {
+  required: true,
+});
+const propName = core.getInput("prop_name", {
+  required: true,
+});
+const envName = core.getInput("env_name", {
+  required: true,
+});
+
+console.log("file : " + file);
+console.log("propName : " + propName);
+console.log("envName : " + envName);
+
+core.exportVariable(envName, 'This-is-a-test');
