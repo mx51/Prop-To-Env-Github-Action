edc1a04aef5ebf6c2bb6e8c38d0185170b71396b
Andrew Kelly <andy@mx51.io>
2022-09-17 02:14:29 +1000

    Debug

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ console.log("envName : " + envName);
 const dotenv = require('dotenv').config({path: file})
 Object.keys(dotenv).forEach(function (key) {
     if (key === propName) {
+        console.log(envName + "=" + parsed[key]);
+
         core.exportVariable(envName, parsed[key]);
         core.setOutput(envName, parsed[key]);
     }
